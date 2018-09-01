706ef45f67e7052edc634ff105eee1b07f1641eb
Giannini Charles <12gcharles@gmail.com>
2018-09-01 19:16:47 -0400

    1 hr commit: set up responsive background color animation and heading text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes,{enableTracing: false}),
+    HttpModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
