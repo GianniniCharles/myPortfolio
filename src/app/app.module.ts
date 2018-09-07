5d8f53388d587f3ca52d6de25980da63a0810aaf
Giannini Charles <12gcharles@gmail.com>
2018-09-07 03:34:51 -0400

    Portfolio site now transitions between pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/landing', pathMatch: 'full'},
-  {path: 'landing', component: LandingComponent},
-  {path: 'home', component: HomeComponent}
+  {path: '', redirectTo:'/landing', pathMatch: 'full', },
+  {path: 'landing', component: LandingComponent, data: {depth: 1}},
+  {path: 'home', component: HomeComponent, data: {depth: 2}}
 ]
 
 @NgModule({
@@ -22,7 +24,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes,{enableTracing: false}),
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
