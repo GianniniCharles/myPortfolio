7ca5ac8d8ffb504d7e5c7e5499c05912ebfa1225
Giannini Charles <12gcharles@gmail.com>
2018-09-08 14:30:31 -0400

    Fixed page transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,30 +10,30 @@ import { trigger, transition, group, query, style, animate} from '@angular/anima
   templateUrl: './app.component.html',
   animations: [
     trigger('routeAnimation', [
-        transition('1 => 2, 2 => 3', [
+        transition('1 => 2', [
             style({ height: '!' }),
-            query(':enter', style({ transform: 'translateX(100%)' })),
+            query(':enter', style({ opacity: 0 })),
             query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
             // animate the leave page away
             group([
                 query(':leave', [
-                    animate('0.5s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(-100%)' })),
+                    animate('2s cubic-bezier(.35,0,.25,1)', style({ opacity: 0 })),
                 ]),
                 // and now reveal the enter
-                query(':enter', animate('0.5s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
+                query(':enter', animate('2s 1s cubic-bezier(.35,0,.25,1)', style({ opacity: 1 }))),
             ]),
         ]),
-        transition('3 => 2, 2 => 1', [
+        transition('2 => 1', [
             style({ height: '!' }),
-            query(':enter', style({ transform: 'translateX(-100%)' })),
+            query(':enter', style({ opacity: 1})),
             query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
             // animate the leave page away
             group([
                 query(':leave', [
-                    animate('0.5s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(100%)' })),
+                    animate('2s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(100%)' })),
                 ]),
                 // and now reveal the enter
-                query(':enter', animate('0.5s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
+                query(':enter', animate('2s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
             ]),
         ]),
     ])
